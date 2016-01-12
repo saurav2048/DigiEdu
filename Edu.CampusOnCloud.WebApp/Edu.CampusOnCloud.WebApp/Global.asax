ddad360db8233b801b541f741cfbbd2e8268f0ee
Aniruddha Brahmamurthy <anibmurthy@gmail.com>
2016-01-12 12:28:29 -0500

    Initial build for web app

---
+++ Edu.CampusOnCloud.WebApp/Edu.CampusOnCloud.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Edu.CampusOnCloud.WebApp.MvcApplication" Language="C#" %>
---
+++ Edu.CampusOnCloud.WebApp/Edu.CampusOnCloud.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Edu.CampusOnCloud.WebApp.Startup))]
+namespace Edu.CampusOnCloud.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
