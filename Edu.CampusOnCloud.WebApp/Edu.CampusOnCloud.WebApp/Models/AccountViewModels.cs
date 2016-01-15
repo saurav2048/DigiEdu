6bfa17f4b0d3c51ddeefc2d56e0ae719bb33ecf0
Aniruddha Brahmamurthy <anibmurthy@gmail.com>
2016-01-15 10:10:32 -0500

    Login and Register pages done Added font-awesome updated site css and bundle config Accountviewmodel changed to add new user fields

--- Edu.CampusOnCloud.WebApp/Edu.CampusOnCloud.WebApp/Models/AccountViewModels.cs
+++ Edu.CampusOnCloud.WebApp/Edu.CampusOnCloud.WebApp/Models/AccountViewModels.cs
@@ -65,6 +65,18 @@ namespace Edu.CampusOnCloud.WebApp.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
